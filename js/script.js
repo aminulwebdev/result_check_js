6a0dc9e8da242e545942b2c63d57f1f806ad686c
AminulAnik <imaminul012@gmail.com>
2024-01-11 12:20:30 +0600

    gpa 305 updated

--- js/script.js
+++ js/script.js
@@ -20,20 +20,20 @@ submitBtn.addEventListener("click", function () {
     } else if (getInput.value >= 70 && getInput.value < 80) {
         CheckResult.innerHTML = `I am GPA 4 ( A )`
         images.src = "images/m70.jpg"
-    } else if (getInput.value >= 60 && getInput.value > 70) {
+    } else if (getInput.value >= 60 && getInput.value < 70) {
         CheckResult.innerHTML = `I am GPA 3.50 ( A- )`
         images.src = "images/m60.jpg"
     } else if (getInput.value >= 50 && getInput.value < 60) {
         CheckResult.innerHTML = `I am GPA 3 ( B )`
         images.src = "images/m50.jpg"
-    } else if (getInput.value >= 40 && getInput.value < 50){
+    } else if (getInput.value >= 40 && getInput.value < 50) {
         CheckResult.innerHTML = `I am GPA 2 ( C )`
         images.src = "images/m40.jpg"
-    } else if (getInput.value >= 33 && getInput.value < 40){
+    } else if (getInput.value >= 33 && getInput.value < 40) {
         CheckResult.innerHTML = `I am GPA 1 ( D )`
         images.src = "images/m33.jpg"
-    } else{
-        CheckResult.innerHTML =`Tumi fail`
+    } else {
+        CheckResult.innerHTML = `Tumi fail`
         images.src = "images/m0.jpg"
     }
 
